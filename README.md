1ac7686d4f5f75e126240cd827ead95b07d06e89
marsvillager <1968643693@qq.com>
2023-03-15 17:48:36 +0800

    format

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Requirement
 
 - 初始更新困难时建议科学上网，更新后不再需要
-- `requirement.txt`
+- 所需软件包在 `requirement.txt` 文件中
 
 # Preparing
 
-- 另外在 [Google Drive](https://drive.google.com/drive/folders/1e-rdHZCyjCW1VsrJu8dNkWWFELc7J9g1?usp=sharing) 下载 3 个过大的文件：`./prompt/model/en.model`, `./prompt/data/data_train.json`, `./prompt/data/vec_inuse.json`
+- 去除拉取 mitre att&ck 数据时的 VCS root mapping（PyCharm ==> Settings ==> Version Control ==> Directory Mappings）
+- 需另外在 [Google Drive](https://drive.google.com/drive/folders/1e-rdHZCyjCW1VsrJu8dNkWWFELc7J9g1?usp=sharing) 下载 3 个过大的文件：`./prompt/model/en.model`, `./prompt/data/data_train.json`, `./prompt/data/vec_inuse.json`
 - `pip install -r requirement.txt`
 - Update: `python main.py -u`
 
@@ -14,7 +15,8 @@
 ## Global ONE
 
 ```bash
-usage: main.py [-h] [-u UPDATE] [-l LEMMA] [-a ATTACK] [-f FILE] [-t TEST] [-n NUMBER] [-p PROMPT]
+usage: main.py [-h] [-u UPDATE] [-l LEMMA] [-a ATTACK] [-t TF_IDF] [-tn TF_IDF_NUMBER] [-tt TF_IDF_TEST] [-ttn TF_IDF_TEST_NUMBER] [-d DOC2VEC] [-dn DOC2VEC_NUMBER]
+               [-dt DOC2VEC_TEST] [-dtm DOC2VEC_TEST_MODEL] [-dtn DOC2VEC_TEST_NUMBER] [-p PROMPT]
 
 Match Security Rules to Mitre ATT&CK.
 
@@ -27,17 +29,37 @@ optional arguments:
   -a ATTACK, --attack ATTACK
                         Tables that store processed data of Mitre ATT&CK. -(full) means use complete words instead of extracting lemma of words -(xxxStemmer) means
                         different tools used to extract lemma of words
-  -f FILE, --file FILE  Match single security rule.
-  -t TEST, --test TEST  Test all files in directory of input.
-  -n NUMBER, --number NUMBER
+  -t TF_IDF, --tf_idf TF_IDF
+                        Match single security rule by TF-IDF.
+  -tn TF_IDF_NUMBER, --tf_idf_number TF_IDF_NUMBER
+                        Show the first few results ranked depends on TF-IDF.
+  -tt TF_IDF_TEST, --tf_idf_test TF_IDF_TEST
+                        Test all files in directory of input.
+  -ttn TF_IDF_TEST_NUMBER, --tf_idf_test_number TF_IDF_TEST_NUMBER
+                        The number is used as a baseline to determine whether the Security Rule passes the test,eg. tags in top 10 will be considered PASS the test.
+  -d DOC2VEC, --doc2vec DOC2VEC
+                        Match single security rule by Doc2Vec.
+  -dn DOC2VEC_NUMBER, --doc2vec_number DOC2VEC_NUMBER
+                        Show the first few results ranked depends on doc2vec.
+  -dt DOC2VEC_TEST, --doc2vec_test DOC2VEC_TEST
+                        Test all files in directory of input.
+  -dtm DOC2VEC_TEST_MODEL, --doc2vec_test_model DOC2VEC_TEST_MODEL
+                        Choose one model to test.
+  -dtn DOC2VEC_TEST_NUMBER, --doc2vec_test_number DOC2VEC_TEST_NUMBER
                         The number is used as a baseline to determine whether the Security Rule passes the test,eg. tags in top 10 will be considered PASS the test.
   -p PROMPT, --prompt PROMPT
                         Prompt words to supply more accuracy description.
 ```
 
-- Match: `python main.py [-f path]`
-- Test: `python main.py [-t directory] [-n number]`
-- Prompt: `python main.py [-p description]`
+- **Match**
+  - *tf-idf*   `python main.py [-t path] [-tn number]` 
+  - *Doc2vec*   `python main.py [-d path] [-dn number]` 
+
+- **Test**
+  - *tf-idf*  `python main.py [-tt directory] [-ttn number]`
+  - *Doc2vec*   `python main.py [-dt directory] [-dtn number] [-dtm model]`
+
+- **Prompt**  `python main.py [-p description]`
 
 ## Global TWO
 
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import argparse
 import os
 import sys
+import gensim
 import nltk
 import pandas as pd
 import ssl
 
-from mitre_attack.process.package import format_data
 from mitre_attack.process.prepare import update
+from tools.doc2vec import build_document, train_model, show_closest_documents
 from prompt.english_reverse_dictionary import score
+from security_rules.process.process_data import process
 from tools.config import Config
-from tools.evaluation import test_all, test
+from tools.evaluation import test_all
+from tools.rank import show_tf_idf
 
 
 def parse_arguments():
@@ -24,14 +27,31 @@ def parse_arguments():
     # note: Corresponding lemma tools used in Security Rules do not supply api
     #       (location: function stemmer in ./tools/transform.py)
 
-    parser.add_argument('-f', '--file', action='store', default=None, help='Match single security rule.')
+    parser.add_argument('-t', '--tf_idf', action='store', default=None, help='Match single security rule by TF-IDF.')
     # e.g.'Config.SECURITY_RULES_PATH + "/sample/" + "15022_LoginLogoutAtUnusualTime.yml"'
     #     './security_rules/data/sample/15022_LoginLogoutAtUnusualTime.yml'
-
-    parser.add_argument('-t', '--test', action='store', default=None, help='Test all files in directory of input.')
+    parser.add_argument('-tn', '--tf_idf_number', action='store', default=10,
+                        help='Show the first few results ranked depends on TF-IDF.')
+    parser.add_argument('-tt', '--tf_idf_test', action='store', default=None,
+                        help='Test all files in directory of input.')
     # e.g.'Config.SECURITY_RULES_PATH + "/fy22_deliverable/rules/"'
     #     './security_rules/data/fy22_deliverable/rules/'
-    parser.add_argument('-n', '--number', action='store', default=10,
+    parser.add_argument('-ttn', '--tf_idf_test_number', action='store', default=10,
+                        help='The number is used as a baseline to determine whether the Security Rule passes the test,'
+                             'eg. tags in top 10 will be considered PASS the test.')
+
+    parser.add_argument('-d', '--doc2vec', action='store', default=None, help='Match single security rule by Doc2Vec.')
+    # e.g.'Config.SECURITY_RULES_PATH + "/sample/" + "15022_LoginLogoutAtUnusualTime.yml"'
+    #     './security_rules/data/sample/15022_LoginLogoutAtUnusualTime.yml'
+    parser.add_argument('-dn', '--doc2vec_number', action='store', default=10,
+                        help='Show the first few results ranked depends on doc2vec.')
+    parser.add_argument('-dt', '--doc2vec_test', action='store', default=None,
+                        help='Test all files in directory of input.')
+    parser.add_argument('-dtm', '--doc2vec_test_model', action='store', default='Doc2Vec<dbow,d100,n5,mc10,t8>',
+                        help='Choose one model to test.')
+    # e.g.'Config.SECURITY_RULES_PATH + "/fy22_deliverable/rules/"'
+    #     './security_rules/data/fy22_deliverable/rules/'
+    parser.add_argument('-dtn', '--doc2vec_test_number', action='store', default=10,
                         help='The number is used as a baseline to determine whether the Security Rule passes the test,'
                              'eg. tags in top 10 will be considered PASS the test.')
 
@@ -67,22 +87,39 @@ def check_arguments(args):
         format_list: pd.DataFrame = pd.read_csv(Config.OUTPUT_CSV + "mitre_data(full).csv")
     else:
         if not os.path.exists(args.attack):
-            sys.exit('Error: processed data of Mitre ATT&CK does not exist.')
+            sys.exit('Error: Processed data of Mitre ATT&CK does not exist.')
         else:
             format_list: pd.DataFrame = pd.read_csv(args.attack)
 
-    if args.file:
-        print(args.file)
-        if not os.path.exists(args.file):
+    if args.tf_idf:
+        if not os.path.exists(args.tf_idf):
             sys.exit('Error: Security Rule file does not exist.')
         else:
-            test(args.file, format_list, args.lemma)
+            keywords: set[str] = process(args.tf_idf, args.lemma)
+            show_tf_idf(keywords, format_list, int(args.tf_idf_number))
 
-    if args.test:
-        if not os.path.exists(args.test):
+    if args.tf_idf_test:
+        if not os.path.exists(args.tf_idf_test):
             sys.exit('Error: Directory does not exist.')
         else:
-            test_all(args.test, format_list, args.number, args.lemma)
+            test_all(args.tf_idf_test, 'tf-idf', format_list, 'Doc2Vec<dbow,d100,n5,mc10,t8>',
+                     int(args.tf_idf_test_number), args.lemma)
+
+    if args.doc2vec:
+        if not os.path.exists(args.doc2vec):
+            sys.exit('Error: Security Rule file does not exist.')
+        else:
+            all_docs: list = build_document()
+            models: list[gensim.models.doc2vec.Doc2Vec] = train_model(all_docs)
+            keywords: set[str] = process(args.doc2vec, False)  # lemma == False
+            show_closest_documents(models, all_docs, list(keywords), int(args.doc2vec_number))
+
+    if args.doc2vec_test:
+        if not os.path.exists(args.doc2vec_test):
+            sys.exit('Error: Directory does not exist.')
+        else:
+            test_all(args.doc2vec_test, 'doc2vec', format_list, args.doc2vec_test_model, int(args.doc2vec_test_number),
+                     False)  # lemma == False
 
     if args.prompt:
         results = score(args.prompt)
